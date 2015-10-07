c7ed67b2550166b6d78728553aab32a6e29f698e
Javier Gamarra <nhpatt@gmail.com>
2015-10-07 15:19:14 +0200

    cleaning useless code and files and sort new activities

---
+++ app/src/main/java/com/liferay/events/SponsorsFragment.java
@@ -0,0 +1,27 @@
+package com.liferay.events;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author Javier Gamarra
+ */
+public class SponsorsFragment extends Fragment {
+
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.content_sponsors, container, false);
+	}
+
+	public static Fragment newInstance() {
+		SponsorsFragment sponsorsFragment = new SponsorsFragment();
+		Bundle args = new Bundle();
+		sponsorsFragment.setArguments(args);
+		return sponsorsFragment;
+	}
+}
