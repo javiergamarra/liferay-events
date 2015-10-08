8478df7f2f53749a8149c3d8077e5e0e3e9e6403
Javier Gamarra <nhpatt@gmail.com>
2015-10-08 14:40:32 +0200

    save message

--- app/src/main/java/com/liferay/events/AddTalkFragment.java
+++ app/src/main/java/com/liferay/events/AddTalkFragment.java
@@ -2,23 +2,113 @@ package com.liferay.events;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.liferay.mobile.screens.ddl.form.DDLFormListener;
+import com.liferay.mobile.screens.ddl.form.DDLFormScreenlet;
+import com.liferay.mobile.screens.ddl.model.DocumentField;
+import com.liferay.mobile.screens.ddl.model.Record;
+
+import org.json.JSONObject;
+
 /**
  * @author Javier Gamarra
  */
-public class AddTalkFragment extends Fragment {
+public class AddTalkFragment extends Fragment implements DDLFormListener {
 
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.content_add_talk, container, false);
+		View inflate = inflater.inflate(R.layout.content_add_talk, container, false);
+
+		DDLFormScreenlet ddlFormScreenlet = (DDLFormScreenlet) inflate.findViewById(R.id.ddl_form);
+		ddlFormScreenlet.setListener(this);
+
+
+
+		return inflate;
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		_view = getActivity().findViewById(android.R.id.content);
 	}
 
 	public static Fragment newInstance() {
 		return new AddTalkFragment();
 	}
+
+	@Override
+	public void onDDLFormLoaded(Record record) {
+
+	}
+
+	@Override
+	public void onDDLFormRecordLoaded(Record record) {
+
+	}
+
+	@Override
+	public void onDDLFormRecordAdded(Record record) {
+
+		Snackbar.make(_view, "Record added!", Snackbar.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onDDLFormRecordUpdated(Record record) {
+
+	}
+
+	@Override
+	public void onDDLFormLoadFailed(Exception e) {
+
+	}
+
+	@Override
+	public void onDDLFormRecordLoadFailed(Exception e) {
+
+	}
+
+	@Override
+	public void onDDLFormRecordAddFailed(Exception e) {
+
+	}
+
+	@Override
+	public void onDDLFormUpdateRecordFailed(Exception e) {
+
+	}
+
+	@Override
+	public void onDDLFormDocumentUploaded(DocumentField documentField, JSONObject jsonObject) {
+
+	}
+
+	@Override
+	public void onDDLFormDocumentUploadFailed(DocumentField documentField, Exception e) {
+
+	}
+
+	@Override
+	public void loadingFromCache(boolean success) {
+
+	}
+
+	@Override
+	public void retrievingOnline(boolean triedInCache, Exception e) {
+
+	}
+
+	@Override
+	public void storingToCache(Object object) {
+
+	}
+
+	private View _view;
 }
